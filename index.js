6fd2a552ae60acef3086bb44aab52567458682dd
Alkesh Thakor <alkesh.thakor@einfochips.com>
2023-08-19 21:50:57 +0530

    Created Get request API.
    1. Created doSum API which calcuate sum of counter which is passed in
    Query parameter.
    2. Created one default route API which will return sum of 1 to 100
    numbers.
    
    Signed-off-by: Alkesh Thakor <alkesh.thakor@einfochips.com>

--- index.js
+++ index.js
@@ -2,10 +2,42 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-function handleFirstRequest(req,res) {
-    res.send('Hello World! Alkesh here...') 
+function calculateSum(counter) {
+    var sum = 0;
+    for(var i=0; i<=counter; i++) {
+        sum+=i;
+    }
+    return sum;
 }
-app.get('/', handleFirstRequest)
+
+function handleFirstRequest(req,res) {
+    var calculatedSum = calculateSum(100);
+    console.log(calculateSum);
+    var answer = "The sum of 1 to 100 is "+calculatedSum;
+    res.send(answer);
+}
+
+function doSumForQueryParams(req,res) {
+    var counter = req.query.counter;
+    console.log("Query parameter counter: "+counter);
+    var calculatedSum = calculateSum(counter);
+    console.log(calculateSum);
+    var answer = "The sum is "+calculatedSum;
+    res.send(answer);
+}
+
+function doSumForPostRequest(req,res) {
+    var counter = req.query.counter;
+    console.log("Query parameter counter: "+counter);
+    var calculatedSum = calculateSum(counter);
+    console.log(calculateSum);
+    var answer = "The sum is "+calculatedSum;
+    res.send(answer);
+}
+
+app.get('/doSumForQueryParams', doSumForQueryParams) 
+//Default for any route after the / 
+app.get('/:anything', handleFirstRequest) 
 
 function started() {
     console.log(`My first web app listening on port ${port}`)
