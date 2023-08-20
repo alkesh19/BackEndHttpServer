5beadfe0cba6fe1803334f7fdb66ed8c33a667c4
Alkesh Thakor <alkesh.thakor@einfochips.com>
2023-08-20 21:51:54 +0530

    Post request implemented to doSum.
    Changes:
    1. Implemented post request which will parse body and do the sume
    2. Added middleware function to intercept every request.
    
    Signed-off-by: Alkesh Thakor <alkesh.thakor@einfochips.com>

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+
+
+function middleware(req,res, next) {
+    console.log("From inside middleware "+ req.headers.counter);
+    next();
+}
+
+//app.use(middleware);
+app.use(bodyParser.json());
+app.use(middleware);
 
 function calculateSum(counter) {
     var sum = 0;
@@ -27,7 +38,11 @@ function doSumForQueryParams(req,res) {
 }
 
 function doSumForPostRequest(req,res) {
-    var counter = req.query.counter;
+    //var counter = req.query.counter;
+    //var counter = req.headers.counter;
+    console.log(req.body);
+    var counter = req.body.counter;
+
     console.log("Query parameter counter: "+counter);
     var calculatedSum = calculateSum(counter);
     console.log(calculateSum);
@@ -35,9 +50,15 @@ function doSumForPostRequest(req,res) {
     res.send(answer);
 }
 
+//To do sume for query params
 app.get('/doSumForQueryParams', doSumForQueryParams) 
-//Default for any route after the / 
+
+//Default for any route after the
 app.get('/:anything', handleFirstRequest) 
+
+//To do sume for body params
+app.post('/doSumForPostRequest',doSumForPostRequest);
+
 
 function started() {
     console.log(`My first web app listening on port ${port}`)
